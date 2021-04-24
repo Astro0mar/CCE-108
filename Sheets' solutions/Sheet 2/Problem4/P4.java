4f168f2f5aa2ec8f83939a1c963cbb361f86b757
Omar Mahmoud <71356296+astro0mar@users.noreply.github.com>
2021-04-24 18:18:11 +0200

    Update P4.java

--- Sheets' solutions/Sheet 2/Problem4/P4.java
+++ Sheets' solutions/Sheet 2/Problem4/P4.java
@@ -1,1 +1,35 @@
+// A simple Java program to compute
+// sum of series 1/1! + 1/2! + .. + 1/n!
+import java.io.*;
+ 
+public class P4  {
+     
+    // Utility function to find
+    static int factorial(int n)
+    {
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+        res *= i;
+        return res;
+    }
+     
+    // A Simple Function to return value
+    // of 1/1! + 1/2! + .. + 1/n!
+    static double sum(int n)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += 1.0/factorial(i);
+        return sum;
+    }
+ 
+    // Driver program
+    public static void main (String[] args)
+    {
+        int n = 5;
+        System.out.println(sum(n));
+    }
+}
 
+
+//https://www.geeksforgeeks.org/efficient-program-compute-sum-series-11-12-13-14-1n/
