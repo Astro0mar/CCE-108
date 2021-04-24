e9a9f96e8000977ece926cd8535cc48a49dced10
Omar Mahmoud <71356296+astro0mar@users.noreply.github.com>
2021-04-24 16:49:37 +0200

    Update and rename OmarMahmoud2.java to Problem1.java

---
+++ Sheets' solutions/Sheet 2/Problem1.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class Problem1 {
+
+	public static void main(String[] args) {
+		
+		
+   Scanner input = new Scanner(System.in);
+
+   System.out.print("Input the time zone offset to GMT: "); //for Egypt UTC/GMT +2 hours
+	        
+	        long timeZoneChange = input.nextInt();
+	        long totalMilliseconds = System.currentTimeMillis();
+	        long totalSeconds = totalMilliseconds / 1000;
+	        long currentSecond = totalSeconds % 60;
+	        long totalMinutes = totalSeconds / 60;
+	        long currentMinute = totalMinutes % 60;
+	        long totalHours = totalMinutes / 60;        
+	        long currentHour = ((totalHours + timeZoneChange) % 24);
+
+    System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);
+		
+		
+		
+		
+	}
+
+}
