28da24e8494e107f6fec7259b910cf59324d9a88
Omar Mahmoud <71356296+astro0mar@users.noreply.github.com>
2021-04-24 17:56:54 +0200

    Update WhileLoop.java

--- Sheets' solutions/Sheet 2/Problem1/WhileLoop.java
+++ Sheets' solutions/Sheet 2/Problem1/WhileLoop.java
@@ -3,12 +3,8 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 		
-		
-     Scanner input = new Scanner(System.in);
-     System.out.println("Enter an integer number");
-
-		//We will find the factorial of this number
-        int number = input.nextInt();
+	//We will find the factorial of this number
+        int number = 5;
         long fact = 1;
         int i = 1;
         while(i<=number)
